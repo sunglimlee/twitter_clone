3d960c105323c60bfbfe3a501f51ecbb67542b99
sunglimlee <steve.patriot@gmail.com>
2023-03-01 10:42:22 -0500

    AnW 2

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -13,7 +13,7 @@ class HomeView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return const Scaffold(
-      body: Center(child: Text("AnW", style: TextStyle(color: Colors.red),)),
+      body: Center(child: Text("AnW2", style: TextStyle(color: Colors.red),)),
     );
   }
 }
